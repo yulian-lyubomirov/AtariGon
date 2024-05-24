5bb34127a6dcbc652bf24394ff1ef2c990c200c8
Yulian <yuliancenov@gmail.com>
2024-05-24 11:33:37 +0200

    mas cambios ne bugolara

--- agents/bugolara.py
+++ agents/bugolara.py
@@ -13,7 +13,7 @@ class Bugolara(Goshi):
     def __init__(self):
         """Initializes the player with the given name."""
         super().__init__(f'Bugolara')
-        self.my_stones= np.zeros(shape=(19,19), dtype=int)
+        # self.my_stones= np.zeros(shape=(19,19), dtype=int)
     
     def check_liberties_in_empty_positions(self,goban) -> dict:
         """Verifica las libertades en todas las posiciones vacías del tablero.
@@ -60,16 +60,16 @@ class Bugolara(Goshi):
             Ten(ten.row + 1, ten.col),
             Ten(ten.row, ten.col - 1),
             Ten(ten.row, ten.col + 1),
-            Ten(ten.row+1,ten.col+1),
-            Ten(ten.row-1,ten.col-1),
-            Ten(ten.row+1,ten.col-1),
-            Ten(ten.row-1,ten.col+1),
+            # Ten(ten.row + 1, ten.col + 1),
+            # Ten(ten.row - 1, ten.col - 1),
+            # Ten(ten.row + 1, ten.col - 1),
+            # Ten(ten.row - 1, ten.col + 1),
         ]
 
-        # Sum 1 point for each adjacent stone in self.my_stones
+        # Sum 1 point for each adjacent stone that belongs to self
         adjacent_count = sum(
             1 for pos in adjacent_positions
-            if 0 <= pos.row < goban.size and 0 <= pos.col < goban.size and self.my_stones[pos.row, pos.col] == 1
+            if 0 <= pos.row < goban.size and 0 <= pos.col < goban.size and goban.ban[pos.row][pos.col] == self
         )
         reward += adjacent_count
 
@@ -79,13 +79,55 @@ class Bugolara(Goshi):
         if is_corner:
             no_non_self_adjacent_stones = all(
                 0 <= pos.row < goban.size and 0 <= pos.col < goban.size and 
-                (self.my_stones[pos.row, pos.col] == 1 or copy_goban.ban[pos.row][pos.col] is None)
+                (goban.ban[pos.row][pos.col] == self or goban.ban[pos.row][pos.col] is None)
                 for pos in adjacent_positions
             )
             if no_non_self_adjacent_stones:
-                reward += 20
+                reward += 10
+
+        # Reward for blocking an enemy stone
+        for pos in adjacent_positions:
+            if 0 <= pos.row < goban.size and 0 <= pos.col < goban.size and goban.ban[pos.row][pos.col] is not None and goban.ban[pos.row][pos.col] != self:
+                # enemy_stone = goban.ban[pos.row][pos.col]
+                enemy_liberties = self.liberties(goban, pos)
+                if len(enemy_liberties) == 1:
+                    reward += 10  # Reward for leaving an enemy stone with one liberty
+                # Reward for saving a friendly stone from being captured
+
+        for pos in adjacent_positions:
+            if 0 <= pos.row < goban.size and 0 <= pos.col < goban.size and goban.ban[pos.row][pos.col] == self:
+                pre_liberties = self.liberties(goban, pos)
+                if len(pre_liberties) == 1:  # Friendly stone was in danger
+                    post_goban = copy.deepcopy(goban)
+                    post_goban.place_stone(ten, self)
+                    post_liberties = self.liberties(post_goban, pos)
+                    if len(post_liberties) > 1:
+                        reward += 15  # Reward for saving a friendly stone
 
         return reward
+    
+    def get_group(self, goban: Goban, pos: Ten, visited: set[Ten]) -> set[Ten]:
+        group = set()
+        to_visit = [pos]
+        while to_visit:
+            current = to_visit.pop()
+            if current not in visited:
+                visited.add(current)
+                group.add(current)
+                neighbors = [n for n in goban.shūi(current) if goban.ban[n.row][n.col] == goban.ban[pos.row][pos.col]]
+                to_visit.extend(neighbors)
+        return group
+    def liberties(self, goban: Goban, pos: Ten) -> set[Ten]:
+        group = self.get_group(goban, pos, set())
+        liberties = set()
+        for stone in group:
+            neighbors = goban.shūi(stone)
+            for neighbor in neighbors:
+                if goban.ban[neighbor.row][neighbor.col] is None:
+                    liberties.add(neighbor)
+
+    
+        return liberties
     
     def decide(self, goban):
         max_reward = -1000
@@ -99,6 +141,6 @@ class Bugolara(Goshi):
                 max_reward = reward
         
         best_moves = [move for move, reward in move_rewards.items() if reward == max_reward]
-        best_move = random.choice(best_moves)
-        self.my_stones[best_move.row][best_move.col]=1
+        # best_move = random.choice(best_moves)
+        # self.my_stones[best_move.row][best_move.col]=1
         return random.choice(best_moves)
